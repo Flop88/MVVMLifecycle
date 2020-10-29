c530969eabaa24533e8381e2325d296bd628b4fb
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-29 09:48:31 +0300

    Use livedata with VALUE

--- app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
@@ -2,28 +2,27 @@ package ru.mvlikhachev.mvvmlifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.Lifecycle
+import android.widget.TextView
+import androidx.lifecycle.MutableLiveData
 
 class MainActivity : AppCompatActivity() {
 
     private val getData = GetData()
 
+    private var liveDataString = MutableLiveData<String>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        if (lifecycle.currentState == Lifecycle.State.INITIALIZED) {
-            println("INITIALIZED")
-        }
+        liveDataString.value = "Hello live data"
+
+        var testTextView : TextView = findViewById(R.id.testTextView)
+
+
 
     }
 
-    override fun onResume() {
-        if (lifecycle.currentState == Lifecycle.State.STARTED) {
-            println("STARTED")
-        }
-        super.onResume()
-    }
 
 
 }
