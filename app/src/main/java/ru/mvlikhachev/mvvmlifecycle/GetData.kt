1651d92222199f1834a24b620bff0856328a757c
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-29 09:15:09 +0300

    Lifecicle example

---
+++ app/src/main/java/ru/mvlikhachev/mvvmlifecycle/GetData.kt
@@ -0,0 +1,24 @@
+package ru.mvlikhachev.mvvmlifecycle
+
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.OnLifecycleEvent
+
+class GetData: LifecycleObserver {
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+    fun getData() {
+        println("get data")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    fun sendData() {
+        println("send data")
+    }
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_ANY)
+    fun any() {
+        println("Any")
+    }
+
+}
