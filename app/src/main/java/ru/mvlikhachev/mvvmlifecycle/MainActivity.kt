4b58fdb5efae3ab9e7499511c62b54eb86324ecb
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-29 10:27:53 +0300

    Learn how to use MediatorLiveData

--- app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
@@ -6,6 +6,7 @@ import android.view.animation.Transformation
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.Transformations
@@ -15,37 +16,41 @@ import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-
-    var ldString = MutableLiveData<String>()
-    var ldInt = MutableLiveData<Int>()
+    val liveDataString = MutableLiveData<String>()
+    val liveDataString2 = MutableLiveData<String>()
+    val mediatorLiveData = MediatorLiveData<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val stringTextView : TextView = findViewById(R.id.stringTextView)
-        val intTextView : TextView = findViewById(R.id.intTextView)
-        val saveButton : Button = findViewById(R.id.saveButton)
+        val text1TextView : TextView = findViewById(R.id.text1TextView)
+        val text2TextView : TextView = findViewById(R.id.text2TextView)
+        val saveButton1 : Button = findViewById(R.id.saveButton1)
+        val saveButton2 : Button = findViewById(R.id.saveButton2)
         val dataEditText : EditText = findViewById(R.id.dataEditText)
 
-        ldString.value = "ldString"
-        ldInt.value = 1
+        liveDataString.value = "String"
+        liveDataString2.value = "String2"
 
-        stringTextView.text = ldString.value + " String"
-        intTextView.text = ldInt.value.toString() + " Int"
+        mediatorLiveData.addSource(liveDataString,{
+            text1TextView.text = it
+        })
 
-        saveButton.setOnClickListener {
-            ldString = Transformations.map(ldInt) {
-                it.toString()
-            } as MutableLiveData<String>
+        mediatorLiveData.addSource(liveDataString2, {
+            text2TextView.text = it
+        })
 
-            ldString.observe(this, Observer {
-                stringTextView.text = it
-            })
+        mediatorLiveData.observe(this, Observer {
+
+        })
+
+        saveButton1.setOnClickListener {
+            liveDataString.value = dataEditText.text.toString()
         }
-
-
-
+        saveButton2.setOnClickListener {
+            liveDataString2.value = dataEditText.text.toString()
+        }
 
     }
 
