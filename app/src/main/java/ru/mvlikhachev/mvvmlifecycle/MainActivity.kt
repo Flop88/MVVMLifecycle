cee199179bbeb42a4c3cc4b20484339d39b790a9
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-29 09:06:45 +0300

    Create project

---
+++ app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.mvlikhachev.mvvmlifecycle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
