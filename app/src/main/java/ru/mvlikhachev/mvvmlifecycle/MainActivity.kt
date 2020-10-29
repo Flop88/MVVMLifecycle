ea8827564bb229f75070e747fdfdeee9de80df5f
Flop88 <wwwqwwwqwwwqwwwq@gmail.com>
2020-10-29 10:18:29 +0300

    Learn Transformations.map

--- app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
+++ app/src/main/java/ru/mvlikhachev/mvvmlifecycle/MainActivity.kt
@@ -2,49 +2,52 @@ package ru.mvlikhachev.mvvmlifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Transformation
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.Transformations
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
-    val myLiveData = MyLiveData()
-    lateinit var observer : Observer<String>
 
-
-    private var liveDataString = MutableLiveData<String>()
+    var ldString = MutableLiveData<String>()
+    var ldInt = MutableLiveData<Int>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val testText : TextView = findViewById(R.id.testTextView)
+        val stringTextView : TextView = findViewById(R.id.stringTextView)
+        val intTextView : TextView = findViewById(R.id.intTextView)
         val saveButton : Button = findViewById(R.id.saveButton)
         val dataEditText : EditText = findViewById(R.id.dataEditText)
 
-        observer = Observer {
-            testText.text = it
-        }
+        ldString.value = "ldString"
+        ldInt.value = 1
 
+        stringTextView.text = ldString.value + " String"
+        intTextView.text = ldInt.value.toString() + " Int"
 
         saveButton.setOnClickListener {
-            myLiveData.setValueToLiveData(dataEditText.text.toString())
+            ldString = Transformations.map(ldInt) {
+                it.toString()
+            } as MutableLiveData<String>
+
+            ldString.observe(this, Observer {
+                stringTextView.text = it
+            })
         }
+
+
+
+
     }
 
-    override fun onStart() {
-        super.onStart()
-        myLiveData.observe(this, observer)
-    }
-
-    override fun onStop() {
-        super.onStop()
-        myLiveData.removeObserver(observer)
-    }
 
 }
